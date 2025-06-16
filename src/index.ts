1ff65a68e8b847e9fa32d419e189242755d1b319
alisher1119 <ali.xay@mail.ru>
2025-06-16 15:14:26 +0500

    add global export point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+export * from "./types";
+export * from "./enums";
+export * from "./providers";
+export * from "./stores";
+export * from "./hooks";
+export * from "./components";
+
 import "./index.css";
